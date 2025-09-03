89e69f195191f5a6ec8acbab1afc2697d89f883c
Rebecca F <rebecca.fsq011@protonmail.com>
2025-09-03 16:31:19 +0200

    Script that prints ASCII alphabet in lowercase, not followed by a newline.

---
+++ python-if_else_loops_functions/2-print_alphabet.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+alphabet_ascii = 97
+"""
+while alphabet_ascii <=122:
+     print(chr(alphabet_ascii), end=" ")
+     alphabet_ascii += 1
+"""
+for alphabet_ascii in range(97, 123):
+    print(chr(alphabet_ascii), end=" ")
