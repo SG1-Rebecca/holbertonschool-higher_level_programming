77168ae6ccccf3112e3d58e4f8efe7c7c4d391ab
Rebecca F <rebecca.fsq011@protonmail.com>
2025-09-04 00:46:50 +0200

    Script that prints numbers from 0 to 99

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python3
-for numbers in range(0, 100):
-        print(f"{numbers}", end="")
+for numbers in range(100):
+        if numbers < 99:
+                print(f"{numbers:02d}", end=", ")
+        else:
+                print(f"{numbers:02d}")
