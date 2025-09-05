6df4f1ba39f979c3e544b27a180a0484c769b1aa
Rebecca F <rebecca.fsq011@protonmail.com>
2025-09-05 04:23:23 +0200

    :bug: fix: removed comments

--- python-if_else_loops_functions/2-print_alphabet.py
+++ python-if_else_loops_functions/2-print_alphabet.py
@@ -1,9 +1,4 @@
 #!/usr/bin/python3
 alphabet_ascii = 97
-"""
-while alphabet_ascii <=122:
-     print(chr(alphabet_ascii), end=" ")
-     alphabet_ascii += 1
-"""
 for alphabet_ascii in range(97, 123):
     print("{}".format(chr(alphabet_ascii)), end="")
