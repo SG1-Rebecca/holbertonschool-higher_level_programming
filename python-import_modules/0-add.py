60126a93626d0731a016e07de35d5d798e5fa4e9
Rebecca F <rebecca.fsq011@protonmail.com>
2025-09-05 18:49:23 +0200

    Program to import add function and print 1 + 2 = 3

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-from add_0 import add
-    def add(a, b):
-        a = 1
-        b = 2
-        print("{} + {} = {}".format(a, b, add(a, b)))
+import add_0
+a = 1
+b = 2
+result = add_0.add(a, b)
+print("{} + {} = {}".format(a, b, result))
