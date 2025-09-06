0e77beedb04ee9dda15ee728e3123d849b3629af
Rebecca F <rebecca.fsq011@protonmail.com>
2025-09-06 16:12:20 +0200

    :bug: fix: add variable to exclude the script name

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
 import sys
 if __name__ == "__main__":
-    nb_arguments = len(sys.argv)
+    parse_arguments = sys.argv[1:]
+    nb_arguments = len(parse_arguments)
     if nb_arguments == 0:
         print("0 arguments", sep='.')
     elif nb_arguments == 1:
         print("1: argument", sep=":")
-        print("1: {}".format(nb_arguments))
-    # else:
-    #     print()
+        print("1: {}".format(parse_arguments))
+    else:
+         print("{} arguments: ".format(nb_arguments))
     for words in range(1, nb_arguments):
         print("{}: {}".format(words, sys.argv[words]))
