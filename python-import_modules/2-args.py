40889db9aa85d22421f348cda20c953382f3e776
Rebecca F <rebecca.fsq011@protonmail.com>
2025-09-06 22:16:40 +0200

    :bug: fix: indent in else condition

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -9,6 +9,6 @@ if __name__ == "__main__":
         print("1: argument", sep=":")
         print("1: {}".format(sys.argv[1]))
     else:
-         print("{} arguments: ".format(nb_arguments))
-    for words in range(1, nb_arguments):
-        print("{}: {}".format(words, sys.argv[words]))
+        print("{} arguments: ".format(nb_arguments))
+        for words in range(1, nb_arguments):
+            print("{}: {}".format(words, sys.argv[words]))
