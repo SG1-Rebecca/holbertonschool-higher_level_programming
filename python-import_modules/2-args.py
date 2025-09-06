0912503e412b330550f2b8b1531127e5398e26b9
Rebecca F <rebecca.fsq011@protonmail.com>
2025-09-07 00:41:43 +0200

    :bug: fix: Corrected loop range to include the last argument

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -4,11 +4,11 @@ if __name__ == "__main__":
     parse_arguments = sys.argv[1:]
     nb_arguments = len(parse_arguments)
     if nb_arguments == 0:
-        print("0 arguments", end=".\n")
+        print("0 arguments")
     elif nb_arguments == 1:
-        print("1: argument", sep=":")
-        print("1: {}".format(sys.argv[1]))
+        print("1 argument")
+        # print("1: {}".format(sys.argv[1]))
     else:
         print("{} arguments: ".format(nb_arguments))
-        for words in range(1, nb_arguments):
-            print("{}: {}".format(words, sys.argv[words]))
+    for words in range(1, nb_arguments + 1):
+        print("{}: {}".format(words, sys.argv[words]))
