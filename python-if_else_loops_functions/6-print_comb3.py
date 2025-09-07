ff9c41cd9c3c0801109602843589eedd1e7b0b67
Rebecca F <rebecca.fsq011@protonmail.com>
2025-09-07 17:33:51 +0200

    fix: change string format in print function

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -2,6 +2,6 @@
 for first_digit in range(0, 10):
     for second_digit in range(first_digit+1, 10):
         if first_digit == 8 and second_digit == 9:
-            print(f"{first_digit}{second_digit}")
+            print("{}{}".format(first_digit, second_digit))
         else:
-            print(f"{first_digit}{second_digit}", end=", ")
+            print("{}{}".format(first_digit, second_digit), end=", ")
