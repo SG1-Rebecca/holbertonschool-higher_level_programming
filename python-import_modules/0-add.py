56c4f2c70e09caceb83002cf8e107e0d935d19bf
Rebecca F <rebecca.fsq011@protonmail.com>
2025-09-07 18:02:37 +0200

    fix: change format import

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -2,5 +2,5 @@
 a = 1
 b = 2
 if __name__ == "__main__":
-    import add_0
-    print("{} + {} = {}".format(a, b, add_0.add(a, b)))
+    from add_0 import add
+    print("{} + {} = {}".format(a, b, add(a, b)))
