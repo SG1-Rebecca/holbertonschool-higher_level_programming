2e31c51bda3d265f6ca8fbd643ed40d828df852d
Rebecca F <rebecca.fsq011@protonmail.com>
2025-09-08 17:36:28 +0200

    draft

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -1,2 +1,9 @@
 #!/usr/bin/python3
 def no_c(my_string):
+    new_string = ""
+    #Browse my_string
+    for char in my_string:
+    #my_string contain specific character ? (c C)
+        # Yes ==> Remove
+        else:
+            return new_string
