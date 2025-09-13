7c03eecae155ce2eac2352ada32964151a7fed9c
Rebecca F <rebecca.fsq011@protonmail.com>
2025-09-13 20:48:36 +0200

    fix: using sort descending method

--- python-data_structures/3-print_reversed_list_integer.py
+++ python-data_structures/3-print_reversed_list_integer.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-    my_list.reverse()
+    my_list.sort(reverse = True)
     for elements in my_list:
         print("{}".format(elements), end="\n")
