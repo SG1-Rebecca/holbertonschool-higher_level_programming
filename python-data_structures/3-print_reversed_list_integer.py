6bc4471ffe85684ac8b3a50f6ee83da39b324197
Rebecca F <rebecca.fsq011@protonmail.com>
2025-09-14 22:37:12 +0200

    change method and print format

--- python-data_structures/3-print_reversed_list_integer.py
+++ python-data_structures/3-print_reversed_list_integer.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-    len_list = len(my_list)
-    for elements in range(len_list - 1, -1, -1):
-        print("{}".format(my_list[elements]), end="\n")
+    my_list.sort(reverse = True)
+    for elements in my_list:
+        print("{:d}".format(elements))
