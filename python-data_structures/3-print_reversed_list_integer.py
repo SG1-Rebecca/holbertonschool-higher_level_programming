986275bfeec5e7d4b620df313f9f98b0e0d7ac31
Rebecca F <rebecca.fsq011@protonmail.com>
2025-09-14 21:11:58 +0200

    Change loop method

--- python-data_structures/3-print_reversed_list_integer.py
+++ python-data_structures/3-print_reversed_list_integer.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-    my_list.sort(reverse = True)
-    for elements in my_list:
-        print("{}".format(elements), end="\n")
+    len_list = len(my_list)
+    for elements in range(len_list - 1, -1, -1):
+        print("{}".format(my_list[elements]), end="\n")
