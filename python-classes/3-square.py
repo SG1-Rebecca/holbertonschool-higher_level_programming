693fcda556e6073aa5336b36b3fb877ceaf0ef12
Rebecca F <rebecca.fsq011@protonmail.com>
2025-09-16 12:18:20 +0200

    Calculate the area of a square

---
+++ python-classes/3-square.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+class Square:
+    """
+    A class that define a square.
+
+    Attributes:
+            __size: The size of the square.
+    """
+    def __init__(self, size=0):
+        """
+        Initializes a new square instance.
+
+        Args:
+            size (int): size of the square. Default is 0.
+
+        Raises:
+            TypeError if size is not an integer.
+            ValueError if size is less than 0.
+
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self._size = size
+
+    def area(self):
+        """
+        Calculates the area of the square
+
+        Return:
+            The current square area
+        """
+        return self._size ** 2
