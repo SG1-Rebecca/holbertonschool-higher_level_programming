00a8cb818adfb2a747c2b9703d798e1625d0c803
Rebecca F <rebecca.fsq011@protonmail.com>
2025-09-19 23:37:21 +0200

    fix: pycodestyle

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -5,17 +5,19 @@ Square module
 This module define a Square class
 
 """
+
+
 class Square:
     """
     A class that defines a square.
 
     Attributes:
-        __size : The size of the square. 
+        __size : The size of the square.
     """
     def __init__(self, size):
         """
         Initializes a new Square instance.
-    
+
         Args:
             size : The size of the square.
         """
