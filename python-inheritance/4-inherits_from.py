83b60452c4432b6c9339b9eece96770b30fe7051
Rebecca F <rebecca.fsq011@protonmail.com>
2025-09-22 17:31:10 +0200

    draft

---
+++ python-inheritance/4-inherits_from.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+Module 
+"""
+
+
+def inherits_from(obj, a_class):
+    """
+
+
+    Args:
+        obj:
+        a_class:
+
+    Returns:
+        bool: 
+    """
+    return isinstance(obj, a_class)
