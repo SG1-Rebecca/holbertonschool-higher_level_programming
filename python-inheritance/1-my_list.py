d8bc1ff38c243faf5762a8cd544cf89828a13ef5
Rebecca F <rebecca.fsq011@protonmail.com>
2025-09-22 15:55:01 +0200

    Print elements of the list in sorted order

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,1 +1,16 @@
 #!/usr/bin/python3
+"""
+Module class MyList
+"""
+
+
+class MyList(list):
+    """
+    A list class that inherits from list
+    """
+    def print_sorted(self):
+        """
+        Method that print the list sorted in ascending order
+        """
+        sort_list = sorted(self)
+        print(sort_list)
