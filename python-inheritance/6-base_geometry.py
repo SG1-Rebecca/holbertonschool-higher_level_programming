8aca92006e3cf846622778e30af0cf97870e54a5
Rebecca F <rebecca.fsq011@protonmail.com>
2025-09-23 12:25:33 +0200

    feat: Add public instance method that raises an Exception with the message

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+Module BaseGeometry
+"""
+
+
+class BaseGeometry:
+    """
+    A BaseGeometry class
+    """
+    def area(self):
+        """
+        Raises an Exception with the message.
+        """
+        raise Exception("area() is not implemented")
