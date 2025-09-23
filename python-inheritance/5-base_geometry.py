3165113628a8884d2dc86a4dfc75daa826263c54
Rebecca F <rebecca.fsq011@protonmail.com>
2025-09-23 12:16:23 +0200

    feat: Write an empty class

---
+++ python-inheritance/5-base_geometry.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Module BaseGeometry
+"""
+
+
+class BaseGeometry:
+    """
+    A BaseGeometry class
+
+    This class does not have any attributes or method yet.
+    """
+    pass
