a5d0d213c98af86f3bfbfdda0a53159bc26a7bed
Rebecca F <rebecca.fsq011@protonmail.com>
2025-09-24 03:48:34 +0200

    feat: Create Square class inherited from Rectangle

---
+++ python-inheritance/10-square.py
@@ -0,0 +1,80 @@
+#!/usr/bin/python3
+"""
+Module BaseGeometry
+"""
+
+
+class BaseGeometry:
+    """
+    A BaseGeometry class
+    """
+
+    def integer_validator(self, name, value):
+        """
+        Method that validate a positive integer
+
+        Args:
+            name: name of the property to validate
+            value: value to check
+
+        Raises:
+            TypeError is value not an integer.
+            ValueError is value is less or equal to 0.
+        """
+        if not isinstance(value, int):
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
+
+
+class Rectangle(BaseGeometry):
+    """
+    A Rectangle class that inherits from BaseGeometry
+    """
+    def __init__(self, width, height):
+        """
+        Initializes a new rectangle instantiation
+
+        Args:
+            width: width of the rectangle
+            height: height of the rectangle
+
+        """
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
+
+    def area(self):
+        """
+        Calculate the area of the rectangle
+        """
+        return self.__width * self.__height
+
+    def __str__(self):
+        """
+        Return string representation of Rectangle in format
+        """
+        return "[Rectangle] {}/{}".format(self.__width, self.__height)
+
+
+class Square(Rectangle):
+    """
+    A Square class that inherits from Rectangle
+    """
+    def __init__(self, size):
+        """
+        Initializes a new square instantiation
+
+        Args:
+            size: The size of the square
+        """
+        self.__size = size
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+
+    def area(self):
+        """
+        Calculate the area of the square
+        """
+        return self.__size * self.__size
