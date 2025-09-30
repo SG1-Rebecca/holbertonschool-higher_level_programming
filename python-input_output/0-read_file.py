ffc284ea3c8763f5bfcb3e2d7bbdeedac70ee1f7
Rebecca F <rebecca.fsq011@protonmail.com>
2025-09-30 10:52:04 +0200

    :memo: update docstring

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,10 +1,16 @@
 #!/usr/bin/python3
 """
+Reads and prints a file
 """
+
+
 def read_file(filename=""):
     """
     Read a text file and print to stdout
+
+    Args:
+        filename (str): The name of the file to read.
     """
-    with open(filename, 'r', encoding= "UTF8") as f:
+    with open(filename, 'r', encoding="UTF8") as f:
         read_content = f.read()
         print(read_content)
