d1fbb244db5f68fdc10439f69d504adafa4f5aea
Rebecca F <rebecca.fsq011@protonmail.com>
2025-10-01 12:54:08 +0200

    feat: Save object to a file

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -1,10 +1,19 @@
 #!/usr/bin/python3
 import json
 """
+Module to save an object to a JSON file
 """
+
+
 def save_to_json_file(my_obj, filename):
     """
+    Writes an Object to a text file, using a JSON representation
+
+    Args:
+        my_obj (any): The object to serialize
+        filename (any): The name of the file to save using JSON representation
     """
-    
-    with open(filename, 'w', encoding= "UTF8") as f:
+    if isinstance(my_obj, set):
+        my_obj = list(my_obj)
+    with open(filename, 'w', encoding="UTF8") as f:
         json.dump(my_obj, f)
