79497429e7ffd6d8909f4d88721d7584dd62a703
Rebecca F <rebecca.fsq011@protonmail.com>
2025-10-09 21:02:16 +0200

    feat: serve a sample JSON data at /data endpoint

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -18,12 +18,23 @@ class Handler(http.server.BaseHTTPRequestHandler):
         """
         # add condition for if endpoint is /data , /status and /
         if self.path == "/":
-            self.send_response_only(200)
+            self.send_response(200)
             self.send_header("Content-type", "text/plain")
             self.end_headers()
             self.wfile.write(b"Hello, this is a simple API!\n")
 
+        elif self.path == "/data":
+            data = {"name": "John", "age": 30, "city": "New York"}
+            json_data = json.dumps(data).encode("utf-8")
+            self.send_response(200)
+            self.send_header("Content-type", "application/json")
+            self.end_headers()
+            self.wfile.write(json_data)
+
 if __name__ == "__main__":
-    with socketserver.TCPServer(("", PORT), Handler) as httpd:
-        print("serving at port", PORT)
-        httpd.serve_forever()
+    try:
+        with socketserver.TCPServer(("", PORT), Handler) as httpd:
+            print("serving at port", PORT)
+            httpd.serve_forever()
+    except KeyboardInterrupt:
+        print("Server stopped.")
