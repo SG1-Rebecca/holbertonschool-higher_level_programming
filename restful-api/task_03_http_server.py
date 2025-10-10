1621dc84e257fb17a36ec48284c6fe8b7d065d76
Rebecca F <rebecca.fsq011@protonmail.com>
2025-10-10 12:45:57 +0200

    feat: implement /info endpoint

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -38,6 +38,14 @@ class Handler(http.server.BaseHTTPRequestHandler):
             self.end_headers()
             self.wfile.write(b"OK")
 
+        elif self.path == "/info":
+            info = {"version": "1.0", "description": "A simple API built with http.server"}
+            json_info = json.dumps(info).encode()
+            self.send_response(200)
+            self.send_header("Content-type", "application/json")
+            self.end_headers()
+            self.wfile.write(json_info)
+
         else:
             self.send_error(404, "Endpoint not found")
 
