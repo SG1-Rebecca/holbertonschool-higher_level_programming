09473b05c7257b90f73e18546e2811533e7f9e4f
Rebecca F <rebecca.fsq011@protonmail.com>
2025-10-10 16:21:50 +0200

    return to initial code

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -3,17 +3,16 @@
 Module to set up a simple HTTP server.
 """
 
-from http.server import BaseHTTPRequestHandler
+import http.server
 import socketserver
 import json
 
-PORT = 8000
 
-
-class Handler(BaseHTTPRequestHandler):
+class Handler(http.server.BaseHTTPRequestHandler):
     """
     Custon HTTP request handler.
     """
+
     def do_GET(self):
         """
         Handles GET requests based on different endpoints.
@@ -22,11 +21,11 @@ class Handler(BaseHTTPRequestHandler):
             self.send_response(200)
             self.send_header("Content-type", "text/plain")
             self.end_headers()
-            self.wfile.write(b"Hello, this is a simple API!\n")
+            self.wfile.write(b"Hello, this is a simple API!")
 
         elif self.path == "/data":
             data = {"name": "John", "age": 30, "city": "New York"}
-            json_data = json.dumps(data).encode()
+            json_data = json.dumps(data).encode("utf-8")
             self.send_response(200)
             self.send_header("Content-type", "application/json")
             self.end_headers()
@@ -39,7 +38,10 @@ class Handler(BaseHTTPRequestHandler):
             self.wfile.write(b"OK")
 
         elif self.path == "/info":
-            info = {"version": "1.0", "description": "A simple API built with http.server"}
+            info = {
+                "version": "1.0",
+                "description": "A simple API built with http.server"
+                }
             json_info = json.dumps(info).encode()
             self.send_response(200)
             self.send_header("Content-type", "application/json")
@@ -47,19 +49,16 @@ class Handler(BaseHTTPRequestHandler):
             self.wfile.write(json_info)
 
         else:
-            self.send_response(404)
-            self.send_header("Content-type", "application/json")
-            self.end_headers()
-            self.wfile.write(b"Endpoint not found")
-
+            self.send_error(404, "Endpoint not found")
 
 
 if __name__ == "__main__":
+    PORT = 8000
     try:
         with socketserver.TCPServer(("", PORT), Handler) as httpd:
             print(f"Serving at port {PORT}: http://localhost:{PORT}")
             httpd.serve_forever()
     except KeyboardInterrupt:
-        print("\nServer stopped.")
+        print("Server stopped.")
     except OSError as error:
         print(error)
