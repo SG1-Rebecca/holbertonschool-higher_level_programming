b955c97f843455a747414fbee5b9dfc3f3a479de
Rebecca F <rebecca.fsq011@protonmail.com>
2025-10-12 23:23:06 +0200

    Implement /basic-protected endpoint

--- restful-api/task_05_basic_security.py
+++ restful-api/task_05_basic_security.py
@@ -1,13 +1,39 @@
 from flask import Flask
 from flask_httpauth import HTTPBasicAuth
-from werwerkzeug.security import generate_password_hash, check_password_hash
+from werkzeug.security import generate_password_hash, check_password_hash
 
 app = Flask(__name__)
+auth = HTTPBasicAuth()
 
 users = {
-    "user1": {"username": "user1", "password": generate_password_hash("password"), "role": "user"},
-    "admin1": {"username": "admin1", "password": generate_password_hash("password"), "role": "admin"}
+    "user1": {"username": "user1",
+              "password": generate_password_hash("password"),
+              "role": "user"},
+    "admin1": {"username": "admin1",
+               "password": generate_password_hash("password"),
+               "role": "admin"}
 }
+
+
+@auth.verify_password
+def verify_password(username, password):
+    """
+    Verify the username and password.
+    """
+    user = users.get(username)
+    if user and check_password_hash(user.get("password"), password):
+        return user
+
+
+@app.route("/basic-protected", methods=["GET"])
+@auth.login_required
+def basic_protected():
+    """
+    Returns a message if the user provides
+    valid basic authentication credentials
+    """
+    return "Basic Auth: Access Granted"
+
 
 if __name__ == "__main__":
     app.run()
