78d7b9d37643ed2cda63926ef223cd15be2cd26c
Rebecca F <rebecca.fsq011@protonmail.com>
2025-10-12 23:56:58 +0200

    Implement /login endpoint

--- restful-api/task_05_basic_security.py
+++ restful-api/task_05_basic_security.py
@@ -1,9 +1,14 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 from flask_httpauth import HTTPBasicAuth
 from werkzeug.security import generate_password_hash, check_password_hash
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'your_secret_key'
+app.config['JWT_SECRET_KEY'] = 'your_jwt_secret_key'
+
 auth = HTTPBasicAuth()
+jwt = JWTManager(app)
+
 
 users = {
     "user1": {"username": "user1",
@@ -33,6 +38,22 @@ def basic_protected():
     valid basic authentication credentials
     """
     return "Basic Auth: Access Granted"
+
+
+@app.route("/login", methods=["POST"])
+def login():
+    """
+    Users authenticate with their credentials and receive a JWT token.
+    """
+    data = request.get_json()
+    username = data.get("username")
+    password = data.get("password")
+    user = users.get(username)
+    if user and check_password_hash(user.get("password"), password):
+        access_token = create_access_token(identity={"username": username, "role": user['role']})
+        return jsonify(access_token=access_token), 200
+    else:
+        return jsonify({"error": "Invalid credentials"}), 401
 
 
 if __name__ == "__main__":
