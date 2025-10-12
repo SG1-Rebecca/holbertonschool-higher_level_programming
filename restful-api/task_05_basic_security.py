572bb2bfe5eb2a569fd5bcb696a1b3fdfc239aa1
Rebecca F <rebecca.fsq011@protonmail.com>
2025-10-12 23:58:07 +0200

    import flask_jwt_extended

--- restful-api/task_05_basic_security.py
+++ restful-api/task_05_basic_security.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify
 from flask_httpauth import HTTPBasicAuth
 from werkzeug.security import generate_password_hash, check_password_hash
+from flask_jwt_extended import JWTManager, create_access_token
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'your_secret_key'
