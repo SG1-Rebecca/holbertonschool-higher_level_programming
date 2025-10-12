acb377acf4b860884b936b85f9e6bbdfacb86c3d
Rebecca F <rebecca.fsq011@protonmail.com>
2025-10-12 23:03:46 +0200

    Setting up import and run app

---
+++ restful-api/task_05_basic_security.py
@@ -0,0 +1,13 @@
+from flask import Flask
+from flask_httpauth import HTTPBasicAuth
+from werwerkzeug.security import generate_password_hash, check_password_hash
+
+app = Flask(__name__)
+
+users = {
+    "user1": {"username": "user1", "password": generate_password_hash("password"), "role": "user"},
+    "admin1": {"username": "admin1", "password": generate_password_hash("password"), "role": "admin"}
+}
+
+if __name__ == "__main__":
+    app.run()
