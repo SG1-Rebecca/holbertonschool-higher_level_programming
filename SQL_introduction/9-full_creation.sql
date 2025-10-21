deb6ca212973484badc0bc0efaa5dc2245a55beb
Rebecca F <rebecca.fsq011@protonmail.com>
2025-10-21 21:18:35 +0200

    :bug: Add multiple records into the second_table

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -4,3 +4,6 @@ CREATE TABLE IF NOT EXISTS second_table(
     name VARCHAR(256),
     score INT
 );
+
+-- Insert multiple rows into the second table
+INSERT INTO second_table (id, name, score) VALUES (1, "John", 10), (2, "Alex", 3), (3, "Bob", 14), (4, "George", 8);
