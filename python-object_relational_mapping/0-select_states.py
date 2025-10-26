c636befd711b038b3acab2d77453d90f5043fc72
Rebecca F <rebecca.fsq011@protonmail.com>
2025-10-26 19:38:29 +0100

    :bug: removed semicolon

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
+"""
+Script to connects to a MySQL database
+and retrieves all states in ascending order.
+"""
 
 import MySQLdb
 import sys
 
 if __name__ == "__main__":
-    """
-    Script to connects to a MySQL database
-    and retrieves all states in ascending order.
-    """
     # Connect to database
     db = MySQLdb.connect(
         host="localhost",
@@ -21,7 +21,7 @@ if __name__ == "__main__":
     cursor = db.cursor()
 
     # Execute the query
-    cursor.execute("SELECT * FROM states ORDER BY id ASC;")
+    cursor.execute("SELECT * FROM states ORDER BY id ASC")
 
     # Fetch the result
     states = cursor.fetchall()
