32bb3d18274c4d56c7dba5c040319289d248b232
Rebecca F <rebecca.fsq011@protonmail.com>
2025-10-26 15:31:19 +0100

    :sparkles: Filter states by name starting with N

---
+++ python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    """
+    Script to connects to a MySQL database
+    and retrieves all states in ascending order.
+    """
+    # Connect to database
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3]
+    )
+
+    # Create a cursor object
+    cursor = db.cursor()
+
+    # Execute the query
+    cursor.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC;")
+
+    # Fetch the result
+    filter_states_by_n = cursor.fetchall()
+
+    # Browse and print the results
+    for state in filter_states_by_n:
+        print(state)
+
+    # Clean up
+    cursor.close()
+    db.close()
