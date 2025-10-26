81ecfdcd8d9a3d82a10b3a6ce455fe7412952fe0
Rebecca F <rebecca.fsq011@protonmail.com>
2025-10-26 17:39:12 +0100

    :sparkles: Script to prevent SQL Injection

---
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    """
+    Script to connects to a MySQL database
+    and retrieves states matching a the argument.
+    """
+
+    # Connect to database
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3]
+    )
+
+    state_name=sys.argv[4]
+
+    # Create a cursor object
+    cursor = db.cursor()
+
+    # Execute the query to prevent SQL injection
+    cursor.execute("SELECT * FROM states WHERE name = %s ORDER BY id ASC;", (state_name,))
+
+    # Fetch the result
+    filter_states_input = cursor.fetchall()
+
+    # Browse and print the results
+    for state in filter_states_input:
+        print(state)
+
+    # Clean up
+    cursor.close()
+    db.close()
