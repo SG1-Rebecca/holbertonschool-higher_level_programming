197a64b4b083ac791f537d87e53959246b2f24de
Rebecca F <rebecca.fsq011@protonmail.com>
2025-11-12 12:02:05 +0100

    :bug: fix: make script semistandard compliant

--- javascript-dom_manipulation/2-script.js
+++ javascript-dom_manipulation/2-script.js
@@ -1,5 +1,5 @@
 const redHeader = document.getElementById('redHeader');
-redHeader.addEventListener('click', ()=> {
-    const header = document.querySelector('header');
-    header.classList.add('red');
+redHeader.addEventListener('click', () => {
+  const header = document.querySelector('header');
+  header.classList.add('red');
 });
