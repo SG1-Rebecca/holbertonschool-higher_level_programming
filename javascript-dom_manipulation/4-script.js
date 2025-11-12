3726fc05f1b74c6abfdc4f3c29d34f74811beb9d
Rebecca F <rebecca.fsq011@protonmail.com>
2025-11-12 15:13:22 +0100

    :bug: fix: Make script semistandard compliant

--- javascript-dom_manipulation/4-script.js
+++ javascript-dom_manipulation/4-script.js
@@ -1,7 +1,7 @@
-const add_item = document.getElementById('add_item');
-add_item.addEventListener('click', () => {
-  const my_list = document.querySelector('.my_list');
+const addItem = document.getElementById('add_item');
+addItem.addEventListener('click', () => {
+  const myList = document.querySelector('.my_list');
   const li = document.createElement('li');
   li.textContent = 'Item';
-  my_list.append(li);
+  myList.append(li);
 });
