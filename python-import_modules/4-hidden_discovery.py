4e7b145c8bf359a5bfe055617408ea48ae8c34b4
Reb F <rebecca.fsq011@protonmail.com>
2026-01-14 16:33:21 +0000

    :bug: fix

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    from hidden_4 import *
-    p_names = dir()
-    for name in sorted(p_names):
+    import hidden_4
+    for name in sorted(dir(hidden_4)):
         if not name.startswith("__"):
             print(name)
 
