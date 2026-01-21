f9da009e8366de0663d192d548952caf3b41aefe
Rebecca F <rebecca.fsq011@protonmail.com>
2026-01-21 23:07:54 +0100

    :sparkles: feat: Integers division with debug

---
+++ python-exceptions/3-safe_print_division.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def safe_print_division(a, b):
+    try:
+        res_div = a / b
+    except ZeroDivisionError:
+        res_div = None
+    finally:
+        print("Inside result: {} ".format(res_div))
