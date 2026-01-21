55b1ca7d6895fa8eca4b17db5cb82d730e6916ed
Rebecca F <rebecca.fsq011@protonmail.com>
2026-01-21 23:53:49 +0100

    :bug: fix: Remove extra space at the end of the printed string

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -5,5 +5,5 @@ def safe_print_division(a, b):
     except ZeroDivisionError:
         res_div = None
     finally:
-        print("Inside result: {} ".format(res_div))
+        print("Inside result: {}".format(res_div))
     return res_div
