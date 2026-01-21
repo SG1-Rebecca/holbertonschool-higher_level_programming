ba6ed9901f01750e0ae46f1f1e5cf16aa47cdc55
Rebecca F <rebecca.fsq011@protonmail.com>
2026-01-21 13:27:38 +0100

    :sparkles: feat: Print number of elements of a list

---
+++ python-exceptions/0-safe_print_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    nb_element = 0
+    for element in range(x):
+        try:
+            print(my_list[element], end="")
+            nb_element += 1
+        except IndexError:
+            break
+    print()
+    return nb_element
