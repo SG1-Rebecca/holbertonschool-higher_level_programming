d33c62209d0585c823cad4c13ef224fcdeb3fa2d
Rebecca F <rebecca.fsq011@protonmail.com>
2026-01-22 13:12:36 +0100

    :white_check_mark: Print square

---
+++ python-test_driven_development/4-print_square.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+Docstring for 4-print_square
+"""
+
+
+def print_square(size):
+    """
+    Print a square with the character #
+
+    Args:
+        size: The size of the square to be printed
+
+    Return:
+        None
+    """
+    if not isinstance(size, int):
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+    if isinstance(size, float) and size < 0:
+        raise TypeError("size must be an integer")
+
+    for _ in range(size):
+        print("#" * size)
