e8f4e917390af2c7f0dca30fb90108d841953252
Rebecca F <rebecca.fsq011@protonmail.com>
2026-01-24 01:46:39 +0100

    :white_check_mark: Text indentation

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -6,18 +6,28 @@ Module 5-text_indentation
 
 def text_indentation(text):
     """
-    Docstring for text_indentation
-    
-    text: Description
+    Function that prints a text with 2 new lines
+    after each of these characters: ., ? and :
+
+    Args:
+        text (str): The text to be indented
     """
     if not isinstance(text, str):
         raise TypeError("text must be a string")
-    
-    spec_char = ['.', '?', ':']
 
-    for char in text:
-        if char in spec_char:
-            print(char, end="")
+    separators = ".?:"
+    idx = 0
+
+    while idx < len(text):
+        if idx == 0 or (idx > 0 and text[idx - 1] in separators):
+            while idx < len(text) and text[idx] == " ":
+                idx += 1
+            if idx >= len(text):
+                break
+
+        print(text[idx], end="")
+
+        if text[idx] in separators:
             print("\n")
-        else:
-            print(char, end="")
+
+        idx += 1
