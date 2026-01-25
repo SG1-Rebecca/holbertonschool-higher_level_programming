dd4743aea2ba581308c2e5864fec4d3850c033e7
Rebecca F <rebecca.fsq011@protonmail.com>
2026-01-25 18:15:26 +0100

    :recycle: refactor: remove redundant type check for negative floats

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -9,7 +9,7 @@ def print_square(size):
     Print a square with the character #
 
     Args:
-        size: The size of the square to be printed
+        size (int): The size of the square to be printed
 
     Return:
         None
@@ -18,8 +18,6 @@ def print_square(size):
         raise TypeError("size must be an integer")
     if size < 0:
         raise ValueError("size must be >= 0")
-    if isinstance(size, float) and size < 0:
-        raise TypeError("size must be an integer")
 
     for _ in range(size):
         print("#" * size)
