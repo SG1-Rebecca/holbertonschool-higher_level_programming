cd26fe87ea0969bf80d109541e07fba335ef6eee
Rebecca F <rebecca.fsq011@protonmail.com>
2026-01-25 23:12:53 +0100

    :bug: fix: Add a check to determine if the matrix is ​​a non-empty list of non-empty lists.

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -20,10 +20,13 @@ def matrix_divided(matrix, div):
     if div == 0:
         raise ZeroDivisionError("division by zero")
 
-    if not all(isinstance(row, list) for row in matrix):
+    if (not isinstance(matrix, list) or
+            matrix == [] or
+            not all(isinstance(row, list) and row for row in matrix)):
         raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
 
     row_length = len(matrix[0])
+
     for row in matrix:
         if len(row) != row_length:
             raise TypeError("Each row of the matrix must have the same size")
@@ -31,6 +34,4 @@ def matrix_divided(matrix, div):
             if not isinstance(element, (int, float)):
                 raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
 
-    new_matrix = [[round(element / div, 2) for element in row] for row in matrix]
-
-    return new_matrix
+    return [[round(element / div, 2) for element in row] for row in matrix]
