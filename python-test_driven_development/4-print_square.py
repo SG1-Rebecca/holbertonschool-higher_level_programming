c9aa33f0f18553960ed44bfa8470c0ae8d3329a1
Rebecca F <rebecca.fsq011@protonmail.com>
2026-01-25 23:29:59 +0100

    :bug::clown_face:

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -14,7 +14,7 @@ def print_square(size):
     Return:
         None
     """
-    if type(size) is not int:
+    if not isinstance(size, int):
         raise TypeError("size must be an integer")
     if size < 0:
         raise ValueError("size must be >= 0")
