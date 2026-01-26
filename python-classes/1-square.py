1bde328fea7a620011db2ae30f0581472470d76c
Rebecca F <rebecca.fsq011@protonmail.com>
2026-01-26 01:30:05 +0100

    :sparkles: feat: Add size attribute to Square class

---
+++ python-classes/1-square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+Module 1-square
+"""
+
+
+class Square:
+    """
+    A class that defines a square.
+
+    Attributes:
+        size: The size of the square.
+    """
+    def __init__(self, size):
+        """
+        Initializes a new Square instance.
+
+        Args:
+            size: The size of the square.
+        """
+        self.__size = size
