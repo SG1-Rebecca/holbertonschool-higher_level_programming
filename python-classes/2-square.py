a8c4da932aa23365f5363eaa358a13c5a926d589
Rebecca F <rebecca.fsq011@protonmail.com>
2026-01-26 01:49:46 +0100

    :sparkles: feat: add size attribute with validation and default value to Square class

---
+++ python-classes/2-square.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+Module 2-square
+"""
+
+
+class Square:
+    """
+    A class that defines a square.
+
+    Attributes:
+        size (int): The size of the square.
+    """
+    def __init__(self, size=0):
+        """
+        Initializes a new Square instance.
+
+        Args:
+            size (int): The size of the square.Default is 0.
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
+        self.__size = size
