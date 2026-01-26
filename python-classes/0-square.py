298c426f4fb0ae861221fb67a0f94d00aa6a2d08
Rebecca F <rebecca.fsq011@protonmail.com>
2026-01-26 01:07:46 +0100

    :sparkles: Create an empty class Square

---
+++ python-classes/0-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Module 0-square
+"""
+
+
+class Square:
+    """
+    A class that defines a square.
+
+    This class does not have any attributes or methods yet.
+    """
+    pass
