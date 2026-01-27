12562a8f799c1d260d5154746ade7e9e212443a7
Rebecca F <rebecca.fsq011@protonmail.com>
2026-01-27 15:13:50 +0100

    :sparkles: feat: Create empty Rectangle class

---
+++ python-more_classes/0-rectangle.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Module 0-rectangle
+"""
+
+
+class Rectangle:
+    """
+    A class that defines a rectangle.
+
+    This class does not have any attributes or method yet.
+    """
+    pass
