a58bc65eb94168c8af5836703ea4005a91650a22
Rebecca F <rebecca.fsq011@protonmail.com>
2026-02-02 13:15:16 +0100

    :sparkles: feat: Returns True only if obj is exactly an instance of the specified class

---
+++ python-inheritance/2-is_same_class.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+Module 2-is_same_class
+"""
+
+
+def is_same_class(obj, a_class):
+    """
+    Function that checks if an object is exactly
+    an instance of the specified class.
+
+    Args:
+        obj: The object to check.
+        a_class: The class to compare.
+
+    Returns:
+        True if obj is exactly an instance of a_class, False otherwise.
+    """
+    return True if type(obj) is a_class else False
