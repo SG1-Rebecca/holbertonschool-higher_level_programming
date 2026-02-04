a198e4fc1c961b600aa72c7adfa8bc8c5b7c5a45
Rebecca F <rebecca.fsq011@protonmail.com>
2026-02-04 03:18:56 +0100

    :sparkles: feat: add abstract Animal Class and its subclasses

---
+++ python-abc/task_00_abc.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+"""
+Module task_00_abc
+"""
+
+from abc import ABC, abstractmethod
+
+
+class Animal(ABC):
+    """
+    An abstract base class representing an animal
+    """
+    @abstractmethod
+    def sound(self):
+        """
+        An abstract method that should be implemented by subclasses
+        to return the sound made by the animal.
+        """
+        pass
+
+
+class Dog(Animal):
+    """
+    A subclass of Animal representing a dog
+    """
+    def sound(self):
+        """
+        Returns the sound of the dog
+        """
+        return "bark"
+
+
+class Cat(Animal):
+    """
+    A subclass of Animal representing a cat
+    """
+    def sound(self):
+        """
+        Returns the sound of the cat
+        """
+        return "meow"
