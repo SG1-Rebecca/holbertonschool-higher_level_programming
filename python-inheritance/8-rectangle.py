54cef0dc8bc58849425f2ddb3b052a156fc48143
Rebecca F <rebecca.fsq011@protonmail.com>
2026-02-06 22:31:44 +0100

    :bug: fix(__init__)

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -12,15 +12,11 @@ class Rectangle(BaseGeometry):
     """
     def __init__(self, width, height):
         """
-        Initializes a Rectangle with width and height.
+        Initializes a Rectangle with private width and height.
 
         Args:
             width (int): The width of the rectangle.
             height (int): The height of the rectangle.
-
-        Raises:
-            TypeError: If width or height is not an integer.
-            ValueError: If width or height is less than or equal to 0.
         """
         self.integer_validator("width", width)
         self.integer_validator("height", height)
