ab1c4cabf94e7fc6ed7d8272c99e4d0aad6bf32f
Rebecca F <rebecca.fsq011@protonmail.com>
2026-02-11 01:59:22 +0100

    :bug: fix: prevent extra newline by adding print end parameter

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -12,4 +12,4 @@ def read_file(filename=""):
         filename (str): The name of the file to read
     """
     with open(filename, encoding="utf-8") as read_file:
-        print(read_file.read())
+        print(read_file.read(), end="")
