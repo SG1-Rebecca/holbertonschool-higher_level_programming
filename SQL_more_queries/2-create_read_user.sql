94e16637b216dd211f7cb35ba0b753a794b8b7cd
Rebecca F <11508@holbertonstudents.com>
2026-03-05 17:38:26 +0100

    :sparkles: feat(db): create database, create user, and grant SELECT privileges to new user

---
+++ SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,8 @@
+-- Create a new database 
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+
+-- Create a new user with password
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+
+-- Grant only SELECT privileges to user_0d_2
+GRANT SELECT ON *.* TO 'user_0d_2'@'localhost';
