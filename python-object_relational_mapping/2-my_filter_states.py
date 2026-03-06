00f7278f333971e9516417c5f6cc4f627a634262
Rebecca F <11508@holbertonstudents.com>
2026-03-06 21:35:39 +0100

    :sparkles: feat: retrieve states matching given name

---
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+Connects to a MySQL database
+and retrieves all states matching the given name.
+"""
+
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    state_name = sys.argv[4]
+
+    db = MySQLdb.connect(host="localhost", port=3306, user=username, passwd=password, db=database)
+
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM states WHERE name = '{}' ORDER BY id ASC".format(state_name))
+    states = cursor.fetchall()
+
+    for my_filter_state in states:
+        print(my_filter_state)
+
+    cursor.close()
+    db.close()
