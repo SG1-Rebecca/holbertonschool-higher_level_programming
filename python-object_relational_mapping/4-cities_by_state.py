bcbb4a4381248e952f0dd6524102c6390aef484d
Rebecca F <11508@holbertonstudents.com>
2026-03-06 23:27:57 +0100

    ✨ feat(db): list all cities with their state names in ascending order

---
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""
+4-cities_by_state.py
+Lists all cities from the database hbtn_0e_4_usa
+"""
+
+import MySQLdb
+import sys
+
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=username,
+        passwd=password,
+        db=database
+    )
+
+    cursor = db.cursor()
+    query = (
+        "SELECT cities.id, cities.name, states.name FROM cities "
+        "JOIN states ON cities.state_id = states.id ORDER BY cities.id ASC"
+    )
+    cursor.execute(query)
+    cities = cursor.fetchall()
+
+    for city in cities:
+        print(city)
+
+    cursor.close()
+    db.close()
